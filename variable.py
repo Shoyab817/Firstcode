cbd3824a5e2ccff3b0d5b4ce41340a1320a39ab1
Km Shoyab <Kmshoyab817@gmail.com>
2026-05-10 00:31:13 +0600

    first commit

---
+++ variable.py
@@ -0,0 +1,30 @@
+# name = "abc"
+# age = 20
+# # print("hello "+ name)
+# # print(type(name))
+# print(type(age))
+#
+# first_name = "shoyab"
+# last_name = "kishor"
+# full_name = first_name + " " + last_name
+# print("hello "+full_name);
+
+# age = 12
+# age += 1
+# print("your age is "+ str (age))
+
+# height = 234.24
+# print("your height is " + str (height))
+# # print(type(height))
+
+# human = True
+# print("are you a human " + str (human))
+
+# name , age, salary, profession = "kishor", 18, 123333, "doctor"
+# print("My Details: ")
+# print(name)
+# print(age)
+# print(salary)
+# print(profession)
+a = b= c= d= 5
+print(a,b,c,d)
