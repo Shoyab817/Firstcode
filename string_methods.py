fd7fd70dc10fbd9aa6afb92e4a2914f292c74cf0
Km Shoyab <Kmshoyab817@gmail.com>
2026-05-10 16:08:26 +0600

    variables learn

--- string_methods.py
+++ string_methods.py
@@ -1,4 +1,15 @@
-# print("Hello World")
-
+# # print("Hello World")
+#
 name = " lorem ipsum abcd"
-print(len(name))
+# print(len(name))
+#
+print(name.find("e"))
+print(name.count("e"))
+print(name.upper())
+print(name.lower())
+print(name.title())
+print(name.capitalize())
+print(name. isdigit())
+print(name.isdecimal())
+print(name.isalpha())
+print(name*4)
