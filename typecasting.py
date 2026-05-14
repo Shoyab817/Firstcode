fa3eed6a62d6abf4528b95721cef8eaaddfc2904
Km Shoyab <Kmshoyab817@gmail.com>
2026-05-14 23:56:23 +0600

    type casting done

--- typecasting.py
+++ typecasting.py
@@ -1,2 +1,12 @@
-print("hellow rodl")
-print("hello kaka babu")
+x = 2
+y = 3.34
+z = '4 '
+
+
+# print(int(y))
+# print(x)
+# print(z*24 )
+
+print("x is " + str(x))
+print("y is ", y)
+print("z is ", z)
